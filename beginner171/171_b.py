83ae09063f1cf8a9274498118f9237e03c074152
Daiki Hirose <programer.sd131@gmail.com>
2020-06-22 11:23:28 +0900

    commit

--- beginner171/171_b.py
+++ beginner171/171_b.py
@@ -1,3 +1,4 @@
+
 N, K = map(int,input().split())
 
 p = list(map(int, input().split()))
@@ -9,3 +10,8 @@ for i in range(K):
     p.remove(min(p))
 
 print(sum(total))
+
+# https://note.nkmk.me/python-list-sort-sorted/
+# sortedを使ったほうが早い
+# 下記は、sortedでpを昇順（低い順）にして新しいリストを作り、:Kでスライスしている。（[:K]の意味は、最初は0からｋ-1までの値を取得しているという意味。そしてsumで合計を出している。
+# print(sum(sorted(p)[:K]))

1a7867664ced4ac7186c5195a4ca58297422ce12
Daiki Hirose <programer.sd131@gmail.com>
2020-06-25 23:53:37 +0900

    commit

--- beginner168/168_c.py
+++ beginner168/168_c.py
@@ -1,3 +1,7 @@
 # 余弦定理から、a**2 = b**2 + c**2 - 2bccosA
 # ヨビノリの授業動画で三角関数の理解を深める
 
+# import math
+# a,b,h,m = map(int,input().split())
+# q = abs((h+m/60)/12-m/60) * 360
+# print(math.sqrt(a**2+b**2-2*a*b*math.cos(math.radians(q))))

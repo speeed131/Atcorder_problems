e3f7a32964cbe39f23df289a44b2d5e3a1feb0fd
Daiki Hirose <programer.sd131@gmail.com>
2020-07-16 17:33:37 +0900

    commit

--- algorithm/breadth_first_search.py
+++ algorithm/breadth_first_search.py
@@ -4,10 +4,10 @@ tree = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12],
 data = [0]
 
 while len(data) > 0:
-    #__dataを取り出して、出力する
-    pos = data.pop(0) #指定した要素を削除し、要素の値を取得
+    #__dataを取り出して、出力する__
+    pos = data.pop(0) #先頭を削除し、要素の値を取得
     print(pos, end=' ')
-    #__取得した要素の
+    #__取得した要素をdataに入れる__
     for i in tree[pos]:
         data.append(i)
 

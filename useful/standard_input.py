611c90eba3cf85aa7fe5eb948024f5a91bc0d536
Daiki Hirose <programer.sd131@gmail.com>
2020-08-20 12:53:12 +0900

    commit

--- useful/standard_input.py
+++ useful/standard_input.py
@@ -3,7 +3,6 @@
 '''
 
 #___文字列や数値をそのままほしい場合___#
-
 # 文字列 １行１つ
 s = input()
 
@@ -16,16 +15,8 @@ s = int(input())
 # 整数 １行２つ
 n, m = map(int, input().split())
 
-# 整数 複数行
-import sys
-a = []
-for l in sys.stdin:
-    a.append(int(l))
-
-
 
 #___リストでほしい場合___#
-
 # リスト 文字列 １行
 a = input().split()
 
@@ -39,7 +30,6 @@ a = [input() for i in range(n)]
 a = [int(input()) for i in range(n)]
 
 #___二次元配列でほしい場合___#
-
 #２次元配列 文字列 nが指定行 空白区切りで複数行列
 a = [input().split() for l in range(n)]
 
